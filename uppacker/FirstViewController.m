b4816505059151415c706f830b1ec05703d14afa
Matsumura Katsuhiro <BiwaCoder@gmail.com>
2014-11-07 23:46:01 +0900

    画像表示や地図のピン作業の着手

--- uppacker/FirstViewController.m
+++ uppacker/FirstViewController.m
@@ -21,6 +21,23 @@
     // マップの中心地がユーザの現在地を追従するように設定
     [self.map setUserTrackingMode:MKUserTrackingModeFollow];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //self.map addAnnotation;
+    
+    //NSArray* locations = ;
+    
+    /*for (Location* location in locations) {
+        CLLocationCoordinate2D coordinate =
+        CLLocationCoordinate2DMake(location.latitude, location.longitude);
+        CustomAnnotation* pin =
+        [[CustomAnnotation alloc] initWithLocationCoordinate:coordinate
+                                                       title:location.title
+                                                    subtitle:location.subtitle];
+        
+        [self.map addAnnotation:pin];
+    }*/
+    
+    //[self.map addAnnotation];
 }
 
 - (void)didReceiveMemoryWarning {
--- uppacker/RunViewController.m
+++ uppacker/RunViewController.m
@@ -20,16 +20,86 @@
     
     //self.targetPoint = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pari.jpg"]];
     //self.targetPoint.center = CGPointMake(150, 150);
-
     //self.targetPoint.tag = ButtonActionsBehaviorTypeAnimateTrash;
     //[self.view addSubview:self.targetPoint];
+    //self.targetPoint.image = [UIImage imageNamed:@"pari.jpg"];
+    //self.playerImg.image =[UIImage imageNamed:@"dash.png"];
+
     
-    self.targetPoint.image = [UIImage imageNamed:@"pari.jpg"];
-    self.playerImg.image =[UIImage imageNamed:@"dash.png"];
+    UIImage *img_mae = [UIImage imageNamed:@"pari.jpg"];  // リサイズ前UIImage
+    UIImage *img_ato;  // リサイズ後UIImage
+    float widthPer = 0.2;  // リサイズ後幅の倍率
+    float heightPer = 0.2;  // リサイズ後高さの倍率
+    CGSize sz = CGSizeMake(img_mae.size.width*widthPer,
+                           img_mae.size.height*heightPer);
+    UIGraphicsBeginImageContext(sz);
+    [img_mae drawInRect:CGRectMake(0, 0, sz.width, sz.height)];
+    img_ato = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:img_ato];
+    imageView.center = CGPointMake(40, 100);
+    [self.view addSubview:imageView];
     
+    
+    UIImage *img_mae2 = [UIImage imageNamed:@"dash.png"];  // リサイズ前UIImage
+    UIImage *img_ato2;  // リサイズ後UIImage
+    float widthPer2 = 0.2;  // リサイズ後幅の倍率
+    float heightPer2 = 0.2;  // リサイズ後高さの倍率
+    CGSize sz2 = CGSizeMake(img_mae2.size.width*widthPer2,
+                           img_mae2.size.height*heightPer2);
+    UIGraphicsBeginImageContext(sz2);
+    [img_mae2 drawInRect:CGRectMake(0, 0, sz2.width, sz2.height)];
+    img_ato2 = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIImageView *imageView2 = [[UIImageView alloc]initWithImage:img_ato2];
+    imageView2.center = CGPointMake(180, 100);
+    [self.view addSubview:imageView2];
     
     
 }
+
+-(void)viewDidAppear:(BOOL)animated{
+    
+    /*
+    UIImage *img_mae = [UIImage imageNamed:@"pari.jpg"];  // リサイズ前UIImage
+    UIImage *img_ato;  // リサイズ後UIImage
+    float widthPer = 0.2;  // リサイズ後幅の倍率
+    float heightPer = 0.2;  // リサイズ後高さの倍率
+    CGSize sz = CGSizeMake(img_mae.size.width*widthPer,
+                           img_mae.size.height*heightPer);
+    UIGraphicsBeginImageContext(sz);
+    [img_mae drawInRect:CGRectMake(0, 0, sz.width, sz.height)];
+    img_ato = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:img_ato];
+    imageView.center = CGPointMake(50, 100);
+    [self.view addSubview:imageView];*/
+    
+    /*
+    // 画像を表示する
+    UIImage *image = [UIImage imageNamed:@"dash.png"];
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+    imageView.center = CGPointMake(160, 240);
+    
+    [self.view addSubview:imageView];
+    */
+    
+    
+    
+    // アニメーション
+    /*[UIView animateWithDuration:2.5f // アニメーション速度2.5秒
+                          delay:1.0f // 1秒後にアニメーション
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         // 画像を2倍に拡大
+                         imageView.transform = CGAffineTransformMakeScale(2.0, 2.0);
+                         
+                     } completion:^(BOOL finished) {
+                         // アニメーション終了時
+                         NSLog(@"アニメーション終了");
+                     }];*/
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
